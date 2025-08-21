67db59f3791829efd582633043aa3d61d59ad4bb
Leonardo Lima <leonardo.lima@kbase.com.br>
2025-08-21 20:42:34 -0300

    implementar testes unitarios.

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
- */
 class ClientFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Client::class;
+
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'cpf' => $this->faker->numerify('###.###.###-##'),
+            'birth_date' => $this->faker->date(),
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->stateAbbr(),
+            'zip_code' => $this->faker->postcode(),
+            'is_active' => true,
+            'password' => bcrypt('password'),
         ];
     }
 }
